172ce58907f65790570e14461759df514f7f2eda
Davis Ochieng <davisochieng81@gmail.com>
2025-11-21 21:32:56 +0300

    fixed import path for input component in contactPage

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,4 +1,4 @@
-import { Input } from "@/components/ui/input";
+import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/_button";
 
 export default function ContactPage() {
